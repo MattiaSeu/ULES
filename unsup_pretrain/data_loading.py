2fcf142482af4b39b5b39e25853aec4eba29d237
matt <seu.1871547@studenti.uniroma1.it>
2023-10-03 01:25:09 +0200

    Removed unused and dead lines of code from data_loading.py

--- unsup_pretrain/data_loading.py
+++ unsup_pretrain/data_loading.py
@@ -17,44 +17,6 @@ from PIL import ImageOps
 from typing import Any, Callable, Dict, List, Optional, Union, Tuple
 from torchvision.datasets import Cityscapes
 
-city_data_path = os.path.join("~/data", 'cityscapes/')
-dataset = Cityscapes(city_data_path, split='train', mode='fine',
-                     target_type='semantic')
-
-ignore_index = 255
-void_classes = [0, 1, 2, 3, 4, 5, 6, 9, 10, 14, 15, 16, 18, 29, 30, -1]
-valid_classes = [ignore_index, 7, 8, 11, 12, 13, 17, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33]
-class_names = ['unlabelled', 'road', 'sidewalk', 'building', 'wall', 'fence', 'pole', 'traffic_light', \
-               'traffic_sign', 'vegetation', 'terrain', 'sky', 'person', 'rider', 'car', 'truck', 'bus', \
-               'train', 'motorcycle', 'bicycle']
-
-class_map = dict(zip(valid_classes, range(len(valid_classes))))
-n_classes = len(valid_classes)
-
-colors = [[0, 0, 0],
-          [128, 64, 128],
-          [244, 35, 232],
-          [70, 70, 70],
-          [102, 102, 156],
-          [190, 153, 153],
-          [153, 153, 153],
-          [250, 170, 30],
-          [220, 220, 0],
-          [107, 142, 35],
-          [152, 251, 152],
-          [0, 130, 180],
-          [220, 20, 60],
-          [255, 0, 0],
-          [0, 0, 142],
-          [0, 0, 70],
-          [0, 60, 100],
-          [0, 80, 100],
-          [0, 0, 230],
-          [119, 11, 32],
-          ]
-
-label_colours = dict(zip(range(n_classes), colors))
-
 
 class GaussianBlur(object):
     """Gaussian blur augmentation in SimCLR https://arxiv.org/abs/2002.05709"""
@@ -68,34 +30,6 @@ class GaussianBlur(object):
         return x
 
 
-def encode_segmap(mask):
-    # remove unwanted classes and rectify the labels of wanted classes
-    for _voidc in void_classes:
-        mask[mask == _voidc] = ignore_index
-    for _validc in valid_classes:
-        mask[mask == _validc] = class_map[_validc]
-    return mask
-
-
-def decode_segmap(temp):
-    # convert gray scale to color
-    temp = temp.numpy()
-    r = temp.copy()
-    g = temp.copy()
-    b = temp.copy()
-    for l in range(0, n_classes):
-        r[temp == l] = label_colours[l][0]
-        g[temp == l] = label_colours[l][1]
-        b[temp == l] = label_colours[l][2]
-
-    rgb = np.zeros((temp.shape[0], temp.shape[1], 3))
-    rgb[:, :, 0] = r / 255.0
-    rgb[:, :, 1] = g / 255.0
-    rgb[:, :, 2] = b / 255.0
-    return rgb
-
-
-# old version of the dataloader
 class CityDataContrastive(Cityscapes):
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
         image = Image.open(self.images[index]).convert('RGB')
@@ -154,7 +88,7 @@ class CityDataContrastive(Cityscapes):
             else:
                 image_contr, x_crop, y_crop, crop_width, crop_height = transform(image_contr)
                 if x_crop != -1:
-                    target_t = target_t.crop([x_crop, y_crop, x_crop+crop_width, y_crop+crop_height])
+                    target_t = target_t.crop([x_crop, y_crop, x_crop + crop_width, y_crop + crop_height])
         target_t = transform_target(target_t)
 
         data_out = {'image': transform_image(image), 'img_contrastive': image_contr,
@@ -215,6 +149,7 @@ class CityData(Cityscapes):
         # tra['mask'] = tra['mask'].permute(1, 2, 0).squeeze()
         return tra['image'], tra['target']
     # torch.unsqueeze(transformed['mask'], 0)
+
 
 class CityDataPixel(Cityscapes):
 
